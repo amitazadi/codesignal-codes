bf938c27fc3ef6f4b0ba766c5cf17924c2365d4e
Amit <khurjaamit@gmail.com>
2021-12-28 23:07:47 +0530

    absoluteValuesSumMinimization code

---
+++ Arcade/32. absoluteValuesSumMinimization.cpp
@@ -0,0 +1,21 @@
+int solution(vector<int> a) 
+{
+    int ans=INT_MAX;
+    int res;
+    
+    for(int i=0;i<a.size();i++)
+    {
+        int temp=0;
+        
+        for(int j=0;j<a.size();j++)
+        temp+=abs(a[i]-a[j]);
+                
+        if(temp<ans)
+        {
+            res=a[i];
+            ans=temp;
+        }
+    }
+    
+    return res;    
+}
